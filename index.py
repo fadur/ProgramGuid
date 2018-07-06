49bf929f4947b4b4c9560429206daed234e900e1
Feisal <iam@feisal.dk>
2018-07-06 18:53:03 +0200

    incr

--- index.py
+++ index.py
@@ -1,12 +1,18 @@
 import json
 import datetime
+import redis
 
-
+client = redis.Redis(os.get('REDIS_HOST'))
+key = 'ProgramGuid'
 def handler(event, context):
+    if not r.get(key):
+        r.set(key, 70200000000)
+    _next = r.incr(key)
     data = {
-        'output': 'Hello World',
+        'next': _next,
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
+    red
     return {'statusCode': 200,
             'body': json.dumps(data),
             'headers': {'Content-Type': 'application/json'}}

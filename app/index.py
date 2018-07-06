0448a20a9f7870e95fb76180196cbc28fbe83eaa
Feisal <iam@feisal.dk>
2018-07-06 19:35:37 +0200

    app

---
+++ app/index.py
@@ -0,0 +1,22 @@
+import os
+import json
+import datetime
+import redis
+
+client = redis.Redis(
+    host=os.environ.get('REDIS_HOST'),
+    port=os.environ.get('REDIST_PORT')
+)
+key = 'ProgramGuid'
+
+def handler(event, context):
+    if not client.get(key):
+        client.set(key, 70200000000)
+    _next = client.incr(key)
+    data = {
+        'next': _next,
+        'timestamp': datetime.datetime.utcnow().isoformat()
+    }
+    return {'statusCode': 200,
+            'body': json.dumps(data),
+            'headers': {'Content-Type': 'application/json'}}
